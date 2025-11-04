80e0d37dc41c34f956aff2b3b8c60df14ca5c1dc
jasonmeigsjr-png <jasonmeigsjr@gmail.com>
2025-11-04 17:53:41 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -14,7 +14,7 @@ def find_max_number(num1, num2, num3):
     else:
       return num3
 
- def find_mean(num1, num2, num3):
+def find_mean(num1, num2, num3):
     H=(num1+num2+num3)/3
     return H
 
